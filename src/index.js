1679f86f0becbe5ed27bdf5cd8ed5f8c333fe2a5
Ben Kimball <bkimball97@gmail.com>
2021-06-01 11:35:26 -0500

    added NavBar and used react route for the first time

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import { Kennel } from './components/Kennel';
+import { BrowserRouter as Router } from "react-router-dom"
+import { Kennel } from './components/Kennels';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Kennel />
-  </React.StrictMode>,
+  <Router>
+      <Kennel />
+  </Router>,
   document.getElementById('root')
 );
 
