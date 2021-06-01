68b38d19d73f9c2eaa5383a14cc188f9353840df
Ben Kimball <bkimball97@gmail.com>
2021-06-01 15:01:02 -0500

    update main branch through chapter 7

--- src/components/customer/CustomerList.js
+++ src/components/customer/CustomerList.js
@@ -19,13 +19,16 @@ export const CustomerList = () => {
       {
         customers.map(customer => {
           return (
-            <div className="customer" id={`customer--${customer.id}`}>
+            <div className="customer" key= {customer.id} id={`customer--${customer.id}`}>
               <div className="customer__name">
                 Name: { customer.name }
               </div>
               <div className="customer__address">
                 Address: { customer.address }
               </div>
+              {/* <div className="customer__animal">
+                Address: { customer.animal.name }
+              </div> */}
             </div>
           )
         })
